c239cd6ce45ab5b464693dff62767b1868d07592
B-MICKIE <153866848+B-6219@users.noreply.github.com>
2025-05-08 22:37:25 +0100

    commit

--- assets/scrpits/scripts.js
+++ assets/scrpits/scripts.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+
+const contactBtn = document.querySelector('.contactBtn');
+const modal = document.getElementById('contactModal');
+const closeBtn = document.querySelector('.close-btn');
+
+contactBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+
+
